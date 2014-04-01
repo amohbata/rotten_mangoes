68a1ee0446809a1c8a023067ffb415170f37cbd9
amohbata <amohbata@gmail.com>
2014-04-01 20:57:36 +0000

    created movies table

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RottenMangoesCd::Application.config.secret_key_base = 'bc3f276c66b1e06251a1c5215d268d27b2982f740b64a7dfd964a013e409f861309c287febd9d15e8e940943ab2243855e7919562c3fa72542991cac73192d21'
